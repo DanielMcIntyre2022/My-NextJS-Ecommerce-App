2d6b455db93dbe7cdc40bfa3fb1949dbbcd2414f
Daniel <daniel-mcintyre@hotmail.com>
2022-10-11 18:50:05 -0400

    created logic for adding products to cart using useContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { ProductsContext, ProductsContextProvider } from '../components/ProductContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProductsContextProvider>
+      <Component {...pageProps} />
+    </ProductsContextProvider>
+  ); 
 }
 
 export default MyApp
