ce7b7062884df5457c78292e7c11546457492998
Daniel <daniel-mcintyre@hotmail.com>
2022-10-11 14:13:13 -0400

    hardcoded the products page to appear on page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,27 @@
 
 export default function Home() {
   return (
-    <div>test</div>
+    <div className="p-5">
+      <div>
+       <h2 className="text-2xl">Mobiles</h2>
+       <div className="py-4">
+        <div className="w-64">
+          <div className="bg-blue-100 p-5 rounded-xl">
+            <img src="/products/iphone.png"/>
+          </div>
+          <div className="mt-2">
+            <h3 className="font-bold text-lg">iphone 14 Pro</h3>
+          </div>
+          <p className="text-sm mt-1 leading-4"> 
+          orem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id pharetra purus, id rutrum risus. Duis tortor sem, aliquet vitae tortor id, lobortis posuere nibh. Aenean enim nibh, consequat sed nulla nec, lacinia ultrices est. Pellentesque egestas.
+          </p>
+          <div className="flex mt-1">
+            <div className="text-2xl font-bold grow">$899</div>
+            <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
+          </div>
+        </div>
+       </div>
+      </div>
+    </div>
   )
 }
