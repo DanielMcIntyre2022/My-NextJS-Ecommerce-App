257865d2897dfc71009a49c67792c6cad456c0b9
Daniel <daniel-mcintyre@hotmail.com>
2022-10-11 17:15:35 -0400

    initial setup of product search feature

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Product from "../components/Product";
 export default function Home() {
 
   const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase, setPhrase] = useState('')
 
   useEffect(() => {
     fetch('/api/products')
@@ -13,14 +14,22 @@ export default function Home() {
 
   const categoriesNames = [...new Set(productsInfo.map(product => product.category))];
   
+  let products;
+  if (phrase) {
+    products = productsInfo.filter(product => product.name.toLowerCase().includes(phrase));
+  } else {
+    products = productsInfo;
+  }
+
   return (
     <div className="p-5">
+      <input value={phrase} onChange={e => setPhrase(e.target.value)}type="text" placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl"></input>
       <div>
         {categoriesNames.map(categoryName => (
           <div key={categoryName}>
                <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
               <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
-               {productsInfo.filter(product => product.category === categoryName).map(productInfo => (
+               {products.filter(product => product.category === categoryName).map(productInfo => (
                 <div key={productInfo._id} className="px-5 snap-start">
                   <Product {...productInfo}/>
                 </div>
