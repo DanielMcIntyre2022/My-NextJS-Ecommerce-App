c1e72713505b7f44a0bf9eb9762294db3c34ee74
Daniel <daniel-mcintyre@hotmail.com>
2022-10-12 11:25:30 -0400

    created initial logic for checkout page and succesfully retrived product data to be displayed on page from the cart

--- pages/api/products.js
+++ pages/api/products.js
@@ -7,7 +7,17 @@ export async function findAllProducts() {
 
 export default async function handle(req, res) {
     await initMongoose();
-    res.json(
-        await findAllProducts()
+    const {ids} = req.query;
+    if (ids) {
+        const idsArray = ids.split(',');
+        res.json( 
+            await Product.find({
+                '_id':{$in:ids.idsArray('')}
+            }).exec()
         );
+    } else {
+        res.json(
+            await findAllProducts()
+            );
+    }   
 }
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,28 @@
+import { useContext, useEffect, useState } from 'react';
 import Layout from '../components/Layout'
+import { ProductsContext } from '../components/ProductContext';
 
 export default function CheckoutPage() {
+
+    const {selectedProducts} = useContext(ProductsContext);
+    const [productsInfos, setProductsInfos] = useState([]);
+
+    useEffect(() => {
+
+        const uniqIds = [...new Set(selectedProducts)];
+        fetch('/api/products?ids='+uniqIds.join(','))
+        .then(res => res.json())
+        .then(json => setProductsInfos(json));
+    },[selectedProducts]);
+
     return (
         <Layout>
-            checkout
+            {!productsInfos.length && (
+                <div>no products in your shopping cart</div>
+            )}
+            {productsInfos.length && (
+                <div></div>
+            )}
         </Layout>
     );
 }
