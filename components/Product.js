b59d97dfa9a931664ebc3bf50243d2a66e23ee04
Daniel <daniel-mcintyre@hotmail.com>
2022-10-12 12:03:38 -0400

    continued to filter through json data to display product, description, cost and quanity on page

--- components/Product.js
+++ components/Product.js
@@ -14,7 +14,7 @@ export default function Product({_id, name, price, description,picture}) {
           <div className="mt-2">
             <h3 className="font-bold text-lg">{name}</h3>
           </div>
-          <p className="text-sm mt-1 leading-4"> 
+          <p className="text-sm mt-1 leading-4 text-gray-500"> 
           {description}
           </p>
           <div className="flex mt-1">
--- pages/checkout.js
+++ pages/checkout.js
@@ -20,8 +20,21 @@ export default function CheckoutPage() {
                 <div>no products in your shopping cart</div>
             )}
             {productsInfos.length && productsInfos.map(productsInfo => (
-                <div>
-                    {productsInfo.name}
+                <div className='flex mb-5'>
+                    <div className='bg-gray-100 p-3 rounded-xl shrink-0'>
+                        <img className='w-24' src={productsInfo.picture} alt=""/>
+                    </div>
+                    <div className='pl-4'>
+                        <h3 className='font-bold text-lg'>{productsInfo.name}</h3>
+                        <p className='text-sm leading-4 text-gray-500'>{productsInfo.description}</p>
+                        <div className='flex'>
+                            <div className='grow'>${productsInfo.price}</div>
+                            <div>
+                                <button className='border border-emerald-500 px-2'>-</button>
+                                {selectedProducts.filter(id => id === productsInfo._id).length}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ))}
         </Layout>
